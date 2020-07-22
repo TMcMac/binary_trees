648968c4922db77b17a9b934903e1163bdc2a1b3
timmcmackenjr@gmail.com <timmcmackenjr@gmail.com>
2020-07-22 19:45:13 +0000

    Intial Commit

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - is a simple program to create nodes
+ * in a binary tree
+ * @parent: is the parent node
+ * @value: is the value that will be assigned to our new node
+ * Return: a new node we have created
+ */
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
+	if (new_node == NULL)
+		return NULL;
+
+	new_node->parent = parent;
+	new_node->n = value;
+	new_node->left = NULL;
+	new_node->right = NULL;
+
+	return (new_node);
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+0x1D. C - Binary trees
+Foundations - Low-level programming & Algorithm ― Data structures and Algorithms
+by Alexandre Gautier, Software Engineer at Holberton, Inc.
+Ongoing project - started 07-20-2020, must end by 07-25-2020
