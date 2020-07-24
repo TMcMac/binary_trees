1e5c5b7ff51bf5455c63eaba6970ba00629a016f
timmcmackenjr@gmail.com <timmcmackenjr@gmail.com>
2020-07-24 01:21:57 +0000

    Adds 7

---
+++ 7-binary_tree_inorder.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_inorder - a short porgram to print binary tree
+ * inorder fashion: left, parent, right.
+ * @tree: originally the root node
+ * @func: a function to print the n value of the current node
+ */
+
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL || func == NULL)
+		return;
+
+	binary_tree_inorder(tree->left, func);
+	func(tree->n);
+	binary_tree_inorder(tree->right, func);
+
+}
