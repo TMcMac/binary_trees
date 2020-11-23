1708b2a56e122195395704e44e5d3328924396c3
Tim <timmcmackenjr@gmail.com>
2020-11-23 17:10:41 -0600

    Betty Fixes

--- binary_tree_print.c
+++ binary_tree_print.c
@@ -21,25 +21,25 @@ static int print_t(const binary_tree_t *tree, int offset, int depth, char **s)
   int width, left, right, is_left, i;
 
   if (!tree)
-    return (0);
+	return (0);
   is_left = (tree->parent && tree->parent->left == tree);
   width = sprintf(b, "(%03d)", tree->n);
   left = print_t(tree->left, offset, depth + 1, s);
   right = print_t(tree->right, offset + left + width, depth + 1, s);
   for (i = 0; i < width; i++)
-    s[depth][offset + left + i] = b[i];
+	s[depth][offset + left + i] = b[i];
   if (depth && is_left)
-    {
-      for (i = 0; i < width + right; i++)
+	{
+	  for (i = 0; i < width + right; i++)
 	s[depth - 1][offset + left + width / 2 + i] = '-';
-      s[depth - 1][offset + left + width / 2] = '.';
-    }
+	  s[depth - 1][offset + left + width / 2] = '.';
+	}
   else if (depth && !is_left)
-    {
-      for (i = 0; i < left + width; i++)
+	{
+	  for (i = 0; i < left + width; i++)
 	s[depth - 1][offset - width / 2 + i] = '-';
-      s[depth - 1][offset + left + width / 2] = '.';
-    }
+	  s[depth - 1][offset + left + width / 2] = '.';
+	}
   return (left + width + right);
 }
 
@@ -71,29 +71,29 @@ void binary_tree_print(const binary_tree_t *tree)
   size_t height, i, j;
 
   if (!tree)
-    return;
+	return;
   height = _height(tree);
   s = malloc(sizeof(*s) * (height + 1));
   if (!s)
-    return;
-  for (i = 0; i < height + 1; i++)
-    {
-      s[i] = malloc(sizeof(**s) * 255);
-      if (!s[i])
 	return;
-      memset(s[i], 32, 255);
-    }
+  for (i = 0; i < height + 1; i++)
+	{
+	  s[i] = malloc(sizeof(**s) * 255);
+	  if (!s[i])
+	return;
+	  memset(s[i], 32, 255);
+	}
   print_t(tree, 0, 0, s);
   for (i = 0; i < height + 1; i++)
-    {
-      for (j = 254; j > 1; --j)
+	{
+	  for (j = 254; j > 1; --j)
 	{
 	  if (s[i][j] != ' ')
-	    break;
+		break;
 	  s[i][j] = '\0';
 	}
-      printf("%s\n", s[i]);
-      free(s[i]);
-    }
+	  printf("%s\n", s[i]);
+	  free(s[i]);
+	}
   free(s);
 }
