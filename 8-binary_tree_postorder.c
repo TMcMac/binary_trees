c29e8080d73a3ec8591b9f3cf8ff400097364b85
Tim <timmcmackenjr@gmail.com>
2020-11-24 13:51:00 -0600

    Adds 8

---
+++ 8-binary_tree_postorder.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_postorder - a function to transverse a binary tree
+ * in postorder fasion
+ * @tree: a pointer to a root node
+ * @func: a function to print the n value of a node
+ **/
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL)
+		return;
+
+	binary_tree_postorder(tree->left, func);
+	binary_tree_postorder(tree->right, func);
+    func(tree->n);
+}
