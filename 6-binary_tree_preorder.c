eb2a5ad7fe69a7d6f33532bfd22bd905f76a26b6
TMcMac <timmcmackenjr@gmail.com>
2020-11-24 17:27:20 +0000

    Adds working 6 and corrects error in print

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -4,14 +4,14 @@
  * binary_tree_preorder - a function to transverse a binary tree
  * in preorder fasion
  * @tree: a pointer to a root node
+ * @func: a function to print the n value of a node
  **/
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree == NULL)
 		return;
-	else 
-		func(tree->n)
-	
+
+	func(tree->n);
 	binary_tree_preorder(tree->left, func);
 	binary_tree_preorder(tree->right, func);
 }
